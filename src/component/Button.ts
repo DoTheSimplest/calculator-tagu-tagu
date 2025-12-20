fae306c860f3461731a681e1cd9ecddeb329cefb
DoTheSimplest <249650346+DoTheSimplest@users.noreply.github.com>
2025-12-21 05:01:55 +0900

    feat: rewrite `Button.js` for tagu-tagu

---
+++ src/component/Button.ts
@@ -0,0 +1,23 @@
+import { button, div } from "tagu-tagu";
+import "./Button.css";
+
+export default function Button(props: {
+	name: string;
+	orange: boolean;
+	wide: boolean;
+	clickHandler: (name: string) => void;
+}) {
+	const handleClick = () => {
+		props.clickHandler(props.name);
+	};
+
+	const className = [
+		"component-button",
+		props.orange ? "orange" : "",
+		props.wide ? "wide" : "",
+	];
+
+	return div({ attr: { class: className.join(" ").trim() } }, [
+		button({ on: { click: handleClick } }, props.name),
+	]);
+}
