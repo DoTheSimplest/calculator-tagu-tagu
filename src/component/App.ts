e9fd3e927b10f1ef3b849c7bdccd801ecfdda94a
DoTheSimplest <249650346+DoTheSimplest@users.noreply.github.com>
2025-12-21 05:40:51 +0900

    feat: rewrite `App.ts` for tagu-tagu

--- src/component/App.ts
+++ src/component/App.ts
@@ -1,4 +1,4 @@
-import { useState } from "tagu-tagu";
+import { div, FromStates, useState } from "tagu-tagu";
 import calculate from "../logic/calculate";
 import ButtonPanel from "./ButtonPanel";
 import Display from "./Display";
@@ -9,14 +9,23 @@ export default function App() {
 		total: null,
 		next: null,
 		operation: null,
+	} as {
+		total?: string | null;
+		next?: string | null;
+		operation?: string | null;
 	});
 
-	const handleClick = (buttonName) => {
+	const handleClick = (buttonName: string) => {
 		state.set(calculate(state.get(), buttonName));
 	};
 
 	return div({ attr: { class: "component-app" } }, [
-		Display({ value: state.next || state.total || "0" }),
+		Display({
+			value: FromStates(
+				[state],
+				() => state.get().next ?? state.get().total ?? "0",
+			),
+		}),
 		ButtonPanel({ clickHandler: handleClick }),
 	]);
 }
--- src/component/Display.ts
+++ src/component/Display.ts
@@ -1,6 +1,6 @@
 import "./Display.css";
-import { div } from "tagu-tagu";
+import { div, type State } from "tagu-tagu";
 
-export default function Display(props: { value: string }) {
+export default function Display(props: { value: State<string> }) {
 	return div({ attr: { class: "component-display" } }, [div(props.value)]);
 }
